b67fc913fb4ac8ed1e09eed1568fe85db8cc07a6
t <cake3327@gmail.com>
2023-09-06 15:08:18 +0300

    update

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,17 +1,11 @@
 import { kv } from '@vercel/kv'
-import { OpenAIStream, StreamingTextResponse } from 'ai'
-import { Configuration, OpenAIApi } from 'openai-edge'
+import { nanoid } from '@/lib/utils'
+import axios from 'axios'
 
 import { auth } from '@/auth'
-import { nanoid } from '@/lib/utils'
 
-// export const runtime = 'edge'
-
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY
-})
-
-const openai = new OpenAIApi(configuration)
+const API_KEY = process.env.OPENAI_API_KEY
+const ENDPOINT = 'https://gpt.darkcoder15.tk/v1'
 
 export async function POST(req: Request) {
   const json = await req.json()
@@ -24,44 +18,49 @@ export async function POST(req: Request) {
     })
   }
 
-  if (previewToken) {
-    configuration.apiKey = previewToken
+  const key = previewToken || API_KEY
+
+  const headers = {
+    'Authorization': `Bearer ${key}`,
+    'Content-Type': 'application/json'
   }
 
-  const res = await openai.createChatCompletion({
+  const res = await axios.post(`${ENDPOINT}/chat`, {
     model: 'gpt-3.5-turbo',
     messages,
     temperature: 0.7,
     stream: true
-  })
+  }, { headers })
 
-  const stream = OpenAIStream(res, {
-    async onCompletion(completion) {
-      const title = json.messages[0].content.substring(0, 100)
-      const id = json.id ?? nanoid()
-      const createdAt = Date.now()
-      const path = `/chat/${id}`
-      const payload = {
-        id,
-        title,
-        userId,
-        createdAt,
-        path,
-        messages: [
-          ...messages,
-          {
-            content: completion,
-            role: 'assistant'
-          }
-        ]
+  const completion = res.data.choices[0].text
+
+  const title = json.messages[0].content.substring(0, 100)
+  const id = json.id ?? nanoid()
+  const createdAt = Date.now()
+  const path = `/chat/${id}`
+  const payload = {
+    id,
+    title,
+    userId,
+    createdAt,
+    path,
+    messages: [
+      ...messages,
+      {
+        content: completion,
+        role: 'assistant'
       }
-      await kv.hmset(`chat:${id}`, payload)
-      await kv.zadd(`user:chat:${userId}`, {
-        score: createdAt,
-        member: `chat:${id}`
-      })
-    }
+    ]
+  }
+    
+  await kv.hmset(`chat:${id}`, payload)
+  await kv.zadd(`user:chat:${userId}`, {
+    score: createdAt,
+    member: `chat:${id}`
   })
 
-  return new StreamingTextResponse(stream)
+  return new Response(JSON.stringify(payload), {
+    headers: { 'Content-Type': 'application/json' },
+  })
 }
+
